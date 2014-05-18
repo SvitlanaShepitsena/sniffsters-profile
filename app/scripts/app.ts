8326a87f2ee2472d0632fd007af5ffaea6a98a61
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-05-18 15:19:13 -0500

    Grunt Remove Controller

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -3,10 +3,8 @@
 /// <reference path="services/DataService.ts" />
 /// <reference path="services/CopyProfileService.ts" />
 /// <reference path="directives/BreederDetails.ts" />
-
 /// <reference path="controllers/EditCtrl.ts" />
 /// <reference path="directives/lookerDetails.ts" />
-/// <reference path="controllers/AboutCtrl.ts" />
 /// <reference path="controllers/PhotosCtrl.ts" />
 /// <reference path="controllers/PuppiesCtrl.ts" />
 /// <reference path="controllers/DetailsCtrl.ts" />
@@ -15,12 +13,6 @@
 //#ref
 
 var profile = angular.module("profile", ['ui.router']);
-
-
-profile.config(function ($httpProvider) {
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
-
 
 profile.service("CopyProfileService", CopyProfileService);
 profile.filter('boolString', () => {
@@ -36,66 +28,54 @@ profile.directive("aboutInfo", aboutInfo);
 profile.directive("breederDetails", breederDetails);
 
 profile.controller("EditCtrl", EditCtrl);
-profile.controller("AboutCtrl", AboutCtrl);
 profile.controller("PhotosCtrl", PhotosCtrl);
 profile.controller("PuppiesCtrl", PuppiesCtrl);
 profile.controller("DetailsCtrl", DetailsCtrl);
 profile.controller("TestimonialsCtrl", TestimonialsCtrl);
 //#ctrl
 
-profile.config(function ($httpProvider) {
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-});
 
 profile.value("toastr", toastr)
 profile.service("DataService", DataService);
 
 profile.config(
-    ["$stateProvider", "$urlRouterProvider",
-        function ($stateProvider, $urlRouterProvider) {
+         ($stateProvider, $urlRouterProvider) =>  {
+//             $urlRouterProvider.when('/test', '/profile/puppies');
+             $urlRouterProvider.otherwise("/profile/about");
 
-            $urlRouterProvider.otherwise("/profile/about");
 
             $stateProvider
                 .state("profile", {
-
+                    abstract: true,
                     url: "/profile",
                     templateUrl: "../views/profile.html"
-
                 })
-
                 .state("profile.about", {
                     url: "/about",
-                    controller: "AboutCtrl",
                     templateUrl: "../views/profile-about.html"
                 })
-
-
-
                 .state("profile.photos", {
                     url: "/photos",
                     controller: "PhotosCtrl",
                     templateUrl: "../views/profile-photos.html"
                 })
+                .state("profile.puppies", {
+                    url: "/puppies",
+                    controller: "PuppiesCtrl",
+                    templateUrl: "../views/profile-puppies.html"
+                })
+                .state("profile.details", {
+                    url: "/details",
+                    controller: "DetailsCtrl",
+                    templateUrl: "../views/profile-details.html"
+                })
+                .state("profile.testimonials", {
+                    url: "/testimonials",
+                    controller: "TestimonialsCtrl",
+                    templateUrl: "../views/profile-testimonials.html"
+                })
 
-
-//                .state("profile.puppies", {
-//                    url: "/puppies",
-//                    controller: "PuppiesCtrl",
-//                    templateUrl: "../views/profile-puppies.html"
-//                })
-//                .state("profile.details", {
-//                    url: "/details",
-//                    controller: "DetailsCtrl",
-//                    templateUrl: "../views/profile-details.html"
-//                })
-//                .state("profile.testimonials", {
-//                    url: "/testimonials",
-//                    controller: "TestimonialsCtrl",
-//                    templateUrl: "../views/profile-testimonials.html"
-//                })
-//
 
 //#state
-        }]);
+        });
 
