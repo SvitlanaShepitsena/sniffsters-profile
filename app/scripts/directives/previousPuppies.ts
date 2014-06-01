c466cc5d8f5e95213c2fa723b6f21c9e39fe1b02
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-06-01 11:17:26 -0500

    Previous Puppies Added

---
+++ app/scripts/directives/previousPuppies.ts
@@ -0,0 +1,27 @@
+/// <reference path="../../bower_components/DefinitelyTyped/angularjs/angular.d.ts" />
+
+interface IPreviousPuppies extends ng.IScope {
+    test:string;
+}
+
+var previousPuppies:() => ng.IDirective = () => {
+
+    return{
+        restrict: 'E',
+        templateUrl: 'views/directives/previous-puppies.html',
+        transclude: true,
+        // replace directive tag with template info
+        replace: true,
+        scope: {
+            ctrl: '=',
+
+            text: '@',
+            func: '&'
+        },
+        link: (scope:IPreviousPuppies, element:ng.IAugmentedJQuery, attrs:ng.IAttributes) => {
+
+
+
+        }
+    }
+}
