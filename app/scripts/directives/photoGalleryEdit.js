3f3841036bbeff66a6f4a7cea14150c9346d08e1
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-06-04 13:01:33 -0500

    Add to commut i

--- app/scripts/directives/photoGalleryEdit.js
+++ app/scripts/directives/photoGalleryEdit.js
@@ -14,10 +14,12 @@ var photoGalleryEdit = function () {
             var index = $stateParams.id;
             $scope.index = index;
 
+            $scope.unloadPriFiles = [];
+
             $scope.onFileSelect = function ($files) {
                 for (var i = 0; i < $files.length; i++) {
                     var file = $files[i];
-                    console.log(file);
+
                     $scope.upload = $upload.upload({
                         url: 'http://localhost:44300/BreederPersonal/AddPicture',
                         data: { gallery: $scope.galleries[$scope.index].Id },
@@ -26,7 +28,7 @@ var photoGalleryEdit = function () {
                     }).success(function (data, status, headers, config) {
                         $scope.galleries[$scope.index].Photos.push({
                             Caption: 'Picture',
-                            FilePath: file.name
+                            FilePath: data.substring(1, data.length - 1)
                         });
                     });
                 }
