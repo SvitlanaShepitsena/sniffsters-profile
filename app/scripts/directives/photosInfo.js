03aeca49fb20d10aec225c44957900b8ae0c0af3
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-06-08 16:35:11 -0500

    ddf

--- app/scripts/directives/photosInfo.js
+++ app/scripts/directives/photosInfo.js
@@ -7,20 +7,29 @@ var photosInfo = function () {
         replace: true,
         controller: function ($scope, $stateParams, $upload, DataService, toastr) {
             $scope.newGallery = {};
-
+            $scope.files = [];
+            var index = 0;
+            $scope.save = function () {
+                $scope.up($scope.files, 0);
+            };
             $scope.onFileSelect = function ($files) {
-                for (var i = 0; i < $files.length; i++) {
-                    var file = $files[i];
+                $scope.files = $files;
+            };
 
-                    $scope.upload = $upload.upload({
-                        url: 'http://localhost:44300/BreederPersonal/AddPictureNewGallery',
-                        data: { Title: $scope.newGallery.Title },
-                        file: file
-                    }).progress(function (evt) {
-                    }).success(function (data, status, headers, config) {
-                        console.log(data);
-                    });
+            $scope.up = function ($files, index) {
+                if (index == $files.length) {
+                    return;
                 }
+                var file = $files[index];
+                $upload.upload({
+                    url: 'http://localhost:44300/BreederPersonal/AddPictureNewGallery',
+                    data: { Title: $scope.newGallery.Title },
+                    file: file
+                }).progress(function (evt) {
+                }).success(function (data, status, headers, config) {
+                    $scope.up($files, index + 1);
+                    console.log(data);
+                });
             };
         }
     };
--- app/scripts/directives/photosInfo.ts
+++ app/scripts/directives/photosInfo.ts
@@ -15,39 +15,48 @@ var photosInfo:() => ng.IDirective = () => {
         // replace directive tag with template info
         replace: true,
 
-        controller: ($scope, $stateParams, $upload, DataService:DataService,toastr:Toastr) => {
+        controller: ($scope, $stateParams, $upload, DataService:DataService, toastr:Toastr) => {
             $scope.newGallery = {};
-
+            $scope.files = [];
+            var index = 0;
+            $scope.save = () => {
+                $scope.up($scope.files, 0);
+            }
             $scope.onFileSelect = ($files) => {
                 //$files: an array of files selected, each file has name, size, and type.
-                for (var i = 0; i < $files.length; i++) {
-                    var file = $files[i];
+//                 var file = $files[0];
+                $scope.files = $files;
+//                $scope.up($files, 0);
 
-                    $scope.upload = $upload.upload({
-                        url: 'http://localhost:44300/BreederPersonal/AddPictureNewGallery',
-                        // method: 'POST' or 'PUT',
-                        // headers: {'header-key': 'header-value'},
-                        // withCredentials: true,
-                        data: {Title: $scope.newGallery.Title},
-                        file: file // or list of files: $files for html5 only
-                        /* set the file formData name ('Content-Desposition'). Default is 'file' */
-                        //fileFormDataName: myFile, //or a list of names for multiple files (html5).
-                        /* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
-                        //formDataAppender: function(formData, key, val){}
-                    }).progress((evt) => {
+            }
+
+            $scope.up = ($files, index) => {
+                if (index == $files.length) {
+                    return;
+                }
+                var file = $files[index];
+                $upload.upload({
+                    url: 'http://localhost:44300/BreederPersonal/AddPictureNewGallery',
+                    // method: 'POST' or 'PUT',
+                    // headers: {'header-key': 'header-value'},
+                    // withCredentials: true,
+                    data: {Title: $scope.newGallery.Title},
+                    file: file // or list of files: $files for html5 only
+                    /* set the file formData name ('Content-Desposition'). Default is 'file' */
+                    //fileFormDataName: myFile, //or a list of names for multiple files (html5).
+                    /* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
+                    //formDataAppender: function(formData, key, val){}
+                }).progress((evt) => {
 //                        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-                    }).success((data, status, headers, config) => {
-                        // file is uploaded successfully
-                        console.log(data);
+                }).success((data, status, headers, config) => {
+                    $scope.up($files, index + 1);
+                    console.log(data);
 //                        $scope.photosCtrl.SelectedGallery.Photos.push(data);
 //                        $scope.myModelObj = {};
 //                        alert(data);
-                    });
-                    //.error(...)
-                    //.then(success, error, progress);
-                    //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
-                }
+                });
             }
+
         }
     }
 }
