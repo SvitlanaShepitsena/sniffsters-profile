f851a1ea4c911cb4cae1dbd341e42151d5b9c2e8
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-06-15 10:13:17 -0500

    Grunt Vs Typescripts errors fixed

--- vs/filters/SpacesToDashes.js
+++
@@ -1,8 +0,0 @@
-var SpacesToDashes = function () {
-    function SpacesToDashes() {
-    }
-    SpacesToDashes.filter = function (value) {
-      return value.replace(/ /g, '-');
-    };
-    return SpacesToDashes;
-  }();
