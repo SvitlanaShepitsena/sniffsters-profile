1204cfa953bb9f392a3a09ce738933201f23d2b2
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-07-21 16:33:15 -0500

    Img-upload directive size canvas

--- app/scripts/directives/svImage.ts
+++ app/scripts/directives/svImage.ts
@@ -17,7 +17,6 @@ var svImage = ($compile, $document) => {
             onFileSelect: '&'
         },
         controller($scope) {
-//            console.log($scope.width);
 
             var realImageWidth:number, realImageHeight:number, scaledImageWidth:number, scaledImageHeight:number;
 
@@ -25,7 +24,7 @@ var svImage = ($compile, $document) => {
                 $scope.realImageWidth = width;
                 $scope.realImageHeight = height;
 
-                $scope.isCropNeeded = ($scope.width < $scope.realImageWidth || $scope.height < $scope.realImageHeight) ? true : false;
+                $scope.isCropNeeded = ( $scope.realImageWidth > $scope.width || $scope.realImageHeight > $scope.height ) ? true : false;
 
             }
             $scope.setScaledImageProp = (width:number) => {
@@ -48,34 +47,33 @@ var svImage = ($compile, $document) => {
 //                console.log('I am herer');
                 elem.remove('canvas');
                 var canvas = document.createElement('canvas');
-//                elem.prepend(canvas);
+                elem.prepend(canvas);
 
                 var context = canvas.getContext('2d');
                 var imageObj = new Image();
 
                 imageObj.onload = function () {
                     // draw cropped image
-                    var sourceX = scope.x / scope.scaledCoefficient;
-                    var sourceY = scope.y / scope.scaledCoefficient;
-                    var sourceWidth = scope.width;
-                    var sourceHeight = scope.height;
+                    var cf = scope.scaledCoefficient;
+                    var sourceX = scope.x / cf;
+                    var sourceY = scope.y / cf;
+                    var sourceWidth = scope.w / cf;
+                    var sourceHeight = scope.h / cf;
 
-                    var destWidth = sourceWidth;
-                    var destHeight = sourceHeight;
+                    var destWidth = scope.realImageWidth;
+                    var destHeight = scope.realImageHeight;
+
                     var destX = 0;
                     var destY = 0;
 
                     context.drawImage(imageObj, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
 
                     scope.$apply(()=> {
-                        scope.i.file64 = canvas.toDataURL('jpg');
+                        scope.i.file64 = canvas.toDataURL('image/jpg');
                         scope.i.isSized = true;
                     })
                 };
-                scope.realImageWidth = scope.width;
-                scope.realImageHeight = scope.height;
                 scope.cropAccept = false;
-                scope.element.remove();
 
                 scope.hasBeenCropped = true;
 //                    console.log(imageObj);
@@ -83,68 +81,37 @@ var svImage = ($compile, $document) => {
             }
             scope.crop = () => {
                 scope.cropAccept = true;
-                console.log(scope.scaledCropWidth);
-                console.log(scope.scaledCropHeight);
-                scope.element = angular.element('<div></div>');
+                scope.img = $('#cropme');
 
-                var startX = 0, startY = 0;
-                scope.x = 0;
-                scope.y = 0;
-                scope.element.css({
-                    position: 'absolute',
-                    opacity: 0.7,
-                    left: '0px',
-                    top: '0px',
-                    border: '1px solid red',
-                    backgroundColor: 'lightgrey',
-                    cursor: 'pointer',
-                    width: scope.scaledCropWidth + 'px',
-                    height: scope.scaledCropHeight + 'px'
+                scope.img.Jcrop({
+                    onChange: (c:any)=> {
+                        scope.x = c.x;
+                        scope.y = c.y;
+                        scope.w = c.w;
+                        scope.h = c.h;
+//                        console.log(c);
+                    },
+
+                    aspectRatio: scope.width / scope.height
                 });
-
-                scope.element.on('mousedown', function (event) {
-                    // Prevent default dragging of selected content
-                    event.preventDefault();
-                    startX = event.pageX - scope.x;
-                    startY = event.pageY - scope.y;
-                    $document.on('mousemove', mousemove);
-                    $document.on('mouseup', mouseup);
-                });
-
-                function mousemove(event) {
-                    scope.y = event.pageY - startY;
-                    scope.x = event.pageX - startX;
-                    if (scope.y < 0) {
-                        scope.y = 0;
-                    }
-                    if (scope.x < 0) {
-                        scope.x = 0;
-                    }
-
-                    if (scope.x + scope.scaledCropWidth > scope.scaledImageWidth) {
-                        scope.x = scope.scaledImageWidth - scope.scaledCropWidth;
-                    }
-
-                    if (scope.y + scope.scaledCropHeight > scope.scaledImageHeight) {
-                        scope.y = scope.scaledImageHeight - scope.scaledCropHeight;
-                    }
-
-                    scope.element.css({
-                        top: scope.y + 'px',
-                        left: scope.x + 'px'
-                    });
-                }
-
-                function mouseup() {
-                    $document.off('mousemove', mousemove);
-                    $document.off('mouseup', mouseup);
-                }
+            }
+            scope.imgTo64 = (src, outputFormat) => {
+                var canvas:any = document.createElement('CANVAS'),
+                    ctx = canvas.getContext('2d'),
+                    img = new Image;
+                img.crossOrigin = 'Anonymous';
+                img.onload = function () {
+                    var dataURL;
+                    canvas.height = img.height;
+                    canvas.width = img.width;
+                    ctx.drawImage(img, 0, 0);
+                    dataURL = canvas.toDataURL(outputFormat);
+                    canvas = null;
+                    return dataURL;
+                };
+                img.src = src;
 
 
-                $compile(scope.element)(scope);
-
-                var imgDiv = elem.find('img').parent();
-                imgDiv.append(scope.element);
             }
 
         }
