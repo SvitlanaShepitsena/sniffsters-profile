f68ddeed48d47ce8c9e979a1e7f299d8bee70d65
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-07-25 09:43:33 -0500

    Merge branch 'master' of C:\App\sniffsters-profile with conflicts.

--- app/scripts/directives/cover.ts
+++ app/scripts/directives/cover.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../bower_components/DefinitelyTyped/angularjs/angular.d.ts" />
-/// <reference path="../../../dist/bower_components/DefinitelyTyped/firebase/firebase.d.ts" />
-/// <reference path="../../../dist/bower_components/DefinitelyTyped/underscore/underscore.d.ts" />
+/// <reference path="../../bower_components/DefinitelyTyped/firebase/firebase.d.ts" />
+/// <reference path="../../bower_components/DefinitelyTyped/underscore/underscore.d.ts" />
 
 var cover:() => ng.IDirective = () => {
 
