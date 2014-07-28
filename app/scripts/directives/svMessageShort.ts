8272afbcc18d033c24669da60736fc3d5a8d7c66
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-07-28 13:02:32 -0500

    Messages From Admin

--- app/scripts/directives/svMessageShort.ts
+++ app/scripts/directives/svMessageShort.ts
@@ -12,14 +12,29 @@ var svMessageShort:() => ng.IDirective = () => {
         scope: {
             index: '=',
             addressat: '=',
-            showMessages: '='
+            showMessages: '=',
+            mainFire: '=',
+            admin: '=',
+            isBreeder: '='
         },
 
-        controller($scope) {
+        controller($scope, toastr) {
 
             $scope.message = {};
 
-            $scope.sendAdminMessage = () => {
+            $scope.sendAdminMessage = (index) => {
+                var messageTo = new Note();
+                messageTo.body = $scope.message.body;
+                messageTo.userName = $scope.admin;
+                messageTo.isTrash = false;
+                messageTo.sent = Date.now();
+                messageTo.amISender = false;
+
+                var userType = $scope.isBreeder ? 'breeders' : 'lookers';
+
+                var receiverMessages = $scope.mainFire.$child(userType).$child($scope.addressat.replace(/\./g, '(p)')).$child('messages');
+                receiverMessages.$add(messageTo);
+
                 var messageFrom = new Note();
                 messageFrom.body = $scope.message.body;
                 messageFrom.userName = $scope.addressat;
@@ -27,7 +42,13 @@ var svMessageShort:() => ng.IDirective = () => {
                 messageFrom.sent = Date.now();
                 messageFrom.amISender = true;
 
-//                var senderMessagesUrl =
+                var senderMessages = $scope.mainFire.$child('admins').$child('messages');
+                senderMessages.$add(messageFrom).then(() => {
+                    toastr.success('Message has been send');
+                    $scope.showMessages.splice(index, 1);
+
+                });
+
 
             }
             $scope.cancelAdminMessage = (index) => {
