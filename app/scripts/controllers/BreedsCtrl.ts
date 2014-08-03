20c39663e8712599a7faed10dc4c9df16c2a8074
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-08-03 15:58:53 -0500

    List of Breeds for Admin

--- app/scripts/controllers/BreedsCtrl.ts
+++ app/scripts/controllers/BreedsCtrl.ts
@@ -1,9 +1,25 @@
 /// <reference path="HomeCtrl.ts" />
+/// <reference path="../app.ts" />
 
 class BreedsCtrl {
 
-    constructor(public $scope, public $state:ng.ui.IStateService, public toastr:Toastr, public $firebase) {
+    constructor(public $scope, $modal, public $stateParams, public $state:ng.ui.IStateService, public toastr:Toastr, public $firebase) {
         $scope.breeds = this;
+        $scope.newBreed = {}
+
+        $scope.saveBreed = (breed:string) => {
+            $scope.breeds.$add(breed);
+        }
+
+        $scope.popoverDelete = {
+            "title": "Delete?",
+            template: '../../views/modals/delete-confirmation.html'
+        };
+        $scope.remove = (key:string)=> {
+            $scope.breeds.$remove(key);
+        }
+        $scope.breeds = $firebase(new Firebase($scope.home.MainUrl + 'breeds'));
+
     }
 
     ShowSuccess(note:string) {
