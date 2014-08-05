dd8d33ea43d484bef880dfd6deb513cb5c10ea3d
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-08-05 13:34:32 -0500

    No litters message bug

--- app/scripts/directives/BreederDetails.js
+++ app/scripts/directives/BreederDetails.js
@@ -29,63 +29,6 @@ var breederDetails = function (FinduserService) {
                 scope.ctrl.Save(scope.ctrl.BreederProfileCopy);
                 scope.IsEdit = false;
             };
-        },
-        controller: function ($scope, $modal, DataService, settings, toastr) {
-            $scope.ShowSuccess = function (note) {
-                toastr.success(note);
-            };
-
-            $scope.message = {};
-            $scope.b = {};
-            $scope.b.profile = {};
-
-            $scope.modalMessage = $modal({
-                "title": "New Message",
-                scope: $scope,
-                show: false,
-                template: "../views/modals/admin-message.html"
-            });
-            $scope.showMessage = function (receiverUserName, receiverNickname) {
-                $scope.b.profile.UserName = receiverUserName;
-                $scope.modalMessage.show();
-            };
-
-            $scope.sendNewMessage = function (sender, addressat, isBreeder) {
-                var body = $scope.message.body;
-                var levelUp = false;
-
-                var to = addressat;
-                FinduserService.find(to).then(function (userToProfile) {
-                    // UserTo is in DB
-                    //                        FROM ##############################
-                    if ($scope.home.isBreeder === true) {
-                        DataService.sendReply($scope.home.userName, userToProfile.Email, userToProfile.UserName, body, true).then(function () {
-                        });
-                    }
-                    if ($scope.home.isBreeder === false) {
-                        DataService.sendLookerReply($scope.home.userName, userToProfile.Email, userToProfile.UserName, body, true).then(function () {
-                        });
-                    }
-
-                    //                TO #################################
-                    if (userToProfile.isBreeder === true) {
-                        DataService.sendReply(userToProfile.Email, $scope.home.userName, $scope.home.nickName, body, false).then(function () {
-                            $scope.ShowSuccess(settings.messageSuccessNotice);
-                        });
-                    }
-                    if (userToProfile.isBreeder === false) {
-                        DataService.sendLookerReply(userToProfile.Email, $scope.home.userName, $scope.home.nickName, body, false).then(function () {
-                            $scope.note.body = "";
-                            $scope.note.to = "";
-                            $scope.reply.body = "";
-
-                            $scope.ShowSuccess(settings.messageSuccessNotice);
-                        });
-                    }
-                });
-
-                $scope.message = {};
-            };
         }
     };
 };
