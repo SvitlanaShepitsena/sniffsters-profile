e63b1b82add287722606937bd034a14d2bbc86e1
Svitlana <svitlana.shepitsena@chicagobusinessintelligence.net>
2014-08-10 14:07:54 -0500

    Sort Reload Works

--- app/scripts/controllers/BreedersCtrl.js
+++ app/scripts/controllers/BreedersCtrl.js
@@ -1,6 +1,7 @@
 /// <reference path="HomeCtrl.ts" />
 var BreedersCtrl = (function () {
     function BreedersCtrl($scope, $modal, $firebase, $stateParams, $state, toastr, DataService) {
+        var _this = this;
         this.$scope = $scope;
         this.$modal = $modal;
         this.$state = $state;
@@ -22,6 +23,8 @@ var BreedersCtrl = (function () {
                 val: '-rating'
             }
         ];
+
+        //                        console.log(breeder.profile.Location);
         $scope.sortFeature = {};
 
         $scope.breedersCtrl = this;
@@ -30,45 +33,48 @@ var BreedersCtrl = (function () {
         $scope.searchBreed = ($stateParams.breed == null || $stateParams.breed == "") ? null : $stateParams.breed;
 
         $scope.breeders = [];
+        $scope.isDataLoading = true;
 
-        var breeders = $firebase(new Firebase($scope.home.MainUrl + 'breeders'));
-        var breedersKeys = breeders.$getIndex();
+        this.$scope.home.auth.$getCurrentUser().then(function (user) {
+            _this.$scope.home.Breedership(_this.$scope.home.FireProcess(user.email)).then(function () {
+                var breeders = $firebase(new Firebase($scope.home.MainUrl + 'breeders'));
+                var breedersKeys = breeders.$getIndex();
 
-        breedersKeys.forEach(function (key) {
-            var breeder = breeders[key];
-            if (!breeder.profile.isAdmin) {
-                if (!_.isNull($scope.searchLocation)) {
-                    //                        console.log($scope.searchLocation);
-                    //                        console.log(breeder.profile.Location);
-                    if (_.isUndefined(breeder.profile) || _.isNull(breeder.profile.Location) || _.isUndefined(breeder.profile.Location) || breeder.profile.Location.indexOf($scope.searchLocation) == -1) {
-                        return;
-                    }
-                }
-                if (!_.isNull($scope.searchBreed)) {
-                    if ((_.isUndefined(breeder.profile.breeds)) || _.values(breeder.profile.breeds).indexOf($scope.searchBreed) == -1) {
-                        console.log(breeder);
-                        return;
-                    }
-                }
-
-                breeder.LittersNumber = breeder.hasOwnProperty('litters') ? _.values(breeder.litters).length : 0;
-
-                if (breeder.hasOwnProperty('feedbacks')) {
-                    var total = 0;
-                    var numb = 0;
-
-                    _.values(breeder.feedbacks).forEach(function (feedback) {
-                        if (feedback.hasOwnProperty('Evaluation') && feedback.Evaluation > 0) {
-                            total += feedback.Evaluation;
-                            numb++;
+                breedersKeys.forEach(function (key) {
+                    var breeder = breeders[key];
+                    if (!breeder.profile.isAdmin) {
+                        if (!_.isNull($scope.searchLocation)) {
+                            if (_.isUndefined(breeder.profile) || _.isNull(breeder.profile.Location) || _.isUndefined(breeder.profile.Location) || breeder.profile.Location.indexOf($scope.searchLocation) == -1) {
+                                return;
+                            }
                         }
-                    });
+                        if (!_.isNull($scope.searchBreed)) {
+                            if ((_.isUndefined(breeder.profile.breeds)) || _.values(breeder.profile.breeds).indexOf($scope.searchBreed) == -1) {
+                                return;
+                            }
+                        }
 
-                    breeder.rating = numb > 0 ? Math.ceil(total / numb) : 0;
-                }
+                        breeder.LittersNumber = breeder.hasOwnProperty('litters') ? _.values(breeder.litters).length : 0;
 
-                $scope.breeders.push(breeder);
-            }
+                        if (breeder.hasOwnProperty('feedbacks')) {
+                            var total = 0;
+                            var numb = 0;
+
+                            _.values(breeder.feedbacks).forEach(function (feedback) {
+                                if (feedback.hasOwnProperty('Evaluation') && feedback.Evaluation > 0) {
+                                    total += feedback.Evaluation;
+                                    numb++;
+                                }
+                            });
+
+                            breeder.rating = numb > 0 ? Math.ceil(total / numb) : 0;
+                        }
+
+                        $scope.breeders.push(breeder);
+                    }
+                });
+                $scope.isDataLoading = false;
+            });
         });
     }
     BreedersCtrl.prototype.ShowSuccess = function (note) {
